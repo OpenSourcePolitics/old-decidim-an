b7046d78b8ed67594493128a84b477ca7fda0069
moustachu <git@moustachu.net>
2020-11-09 22:56:20 +0100

    fix initiatives progress bar threshold

--- app/assets/javascripts/decidim/initiatives/threshold.js
+++ app/assets/javascripts/decidim/initiatives/threshold.js
@@ -1,23 +1,26 @@
 document.addEventListener("DOMContentLoaded",function() {
-  var seuil_1_str = "100 000";
-  var seuil_2_str = "500&nbsp000";
 
-  var seuil_1_int = parseInt(seuil_1_str.replaceAll(" ", ""));
-  var seuil_2_int = parseInt(seuil_2_str.replaceAll("&nbsp", ""));
+  if (window.location.pathname.startsWith("/initiatives")) {
+    var seuil_1_str = "100 000";
+    var seuil_2_str = "500&nbsp000";
 
-  $('.progress__bar').each(function() {
-    var nombre_signatures_str = $(this).find('.progress__bar__number').text();
-    var nombre_signatures_int = parseInt(nombre_signatures_str.replaceAll(" ", ""));
+    var seuil_1_int = parseInt(seuil_1_str.replaceAll(" ", ""));
+    var seuil_2_int = parseInt(seuil_2_str.replaceAll("&nbsp", ""));
 
+    $('.progress__bar').each(function() {
+      var nombre_signatures_str = $(this).find('.progress__bar__number').text();
+      var nombre_signatures_int = parseInt(nombre_signatures_str.replaceAll(" ", ""));
 
-    if (nombre_signatures_int >= seuil_1_int) {
-      $(this).html($(this).html().replaceAll("/" + seuil_1_str, "/" + seuil_2_str));
+      if (nombre_signatures_int >= seuil_1_int) {
+        $(this).html($(this).html().replaceAll("/" + seuil_1_str, "/" + seuil_2_str));
 
-      var progress_bar_value_seuil_1 = $(this).find('.progress.progress__bar__bar').attr('aria-valuenow');
-      var progress_bar_value_seuil_2 = (nombre_signatures_int / seuil_2_int) * 100;
+        var progress_bar_value_seuil_1 = $(this).find('.progress.progress__bar__bar').attr('aria-valuenow');
+        var progress_bar_value_seuil_2 = (nombre_signatures_int / seuil_2_int) * 100;
 
-      $(this).find('.progress.progress__bar__bar').attr('aria-valuenow', progress_bar_value_seuil_2);
-      $(this).html(jQuery(this).html().replaceAll(progress_bar_value_seuil_1 + " percent", progress_bar_value_seuil_2 + " percent").replaceAll(progress_bar_value_seuil_1 + "%", progress_bar_value_seuil_2 + "%"));
-    }
-  });
+        $(this).find('.progress.progress__bar__bar').attr('aria-valuenow', progress_bar_value_seuil_2);
+        $(this).html($(this).html().replaceAll(progress_bar_value_seuil_1 + " percent", progress_bar_value_seuil_2 + " percent").replaceAll(progress_bar_value_seuil_1 + "%", progress_bar_value_seuil_2 + "%"));
+      }
+    });
+  }
+
 });
