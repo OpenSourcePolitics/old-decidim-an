c8e7692d7e210b3fd5e429057c467778d3f7df00
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2021-03-08 13:57:25 +0100

    notify admins when account destroyed (#55)

--- lib/extends/destroy_account_extend.rb
+++ lib/extends/destroy_account_extend.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require "active_support/concern"
+
+require 'active_support/concern'
 
 module DestroyAccountExtend
   extend ActiveSupport::Concern
@@ -9,6 +10,7 @@ module DestroyAccountExtend
       return broadcast(:invalid) unless @form.valid?
 
       Decidim::User.transaction do
+        notify_admins
         manage_user_initiatives
         destroy_user_account!
         destroy_user_authorizations
@@ -20,6 +22,8 @@ module DestroyAccountExtend
       broadcast(:ok)
     end
 
+    private
+
     def manage_user_initiatives
       # Rails.logger.debug "+++++++++++++++++++++++++"
       # Rails.logger.debug "DestroyAccount.manage_user_initiatives"
@@ -30,22 +34,38 @@ module DestroyAccountExtend
       Decidim::Initiative.where(author: @user).each do |initiative|
         # Rails.logger.debug initiative.id
         # Rails.logger.debug initiative.state
-        if %w(created validating accepted).include?(initiative.state)
+        if %w[created validating accepted].include?(initiative.state)
           # Rails.logger.debug "will be discarded"
-          initiative.update_columns(state: "discarded")
+          initiative.update_columns(state: 'discarded')
         else
           # Rails.logger.debug "will be rejected"
-          initiative.update_columns(state: "rejected")
+          initiative.update_columns(state: 'rejected')
         end
         # Rails.logger.debug "--"
       end
+    end
 
+    def notify_admins
+      organization_admins.each do |admin|
+        Decidim::DestroyAccountMailer.notify(admin).deliver_later
+      end
+    end
+
+    # Returns array of administrators with email on notification enabled
+    def organization_admins
+      Decidim::User.where(
+        organization: @user.organization,
+        admin: true,
+        email_on_notification: true
+      ).where.not(id: @user.id)
     end
 
     def destroy_user_authorizations
-      Decidim::Verifications::Authorizations.new(organization: @user.organization, user: @user).query.destroy_all
+      Decidim::Verifications::Authorizations.new(
+        organization: @user.organization,
+        user: @user
+      ).query.destroy_all
     end
-
   end
 end
 
