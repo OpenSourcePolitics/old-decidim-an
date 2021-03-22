dcaa11f5a9cc7c2b9770a7c3366e74e9c981adad
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-03-22 11:07:07 +0100

    Fix initiative state when author destroys account (#59)

--- lib/extends/destroy_account_extend.rb
+++ lib/extends/destroy_account_extend.rb
@@ -11,7 +11,6 @@ module DestroyAccountExtend
 
       Decidim::User.transaction do
         notify_admins
-        manage_user_initiatives
         destroy_user_account!
         destroy_user_authorizations
         destroy_user_identities
@@ -23,27 +22,6 @@ module DestroyAccountExtend
     end
 
     private
-
-    def manage_user_initiatives
-      # Rails.logger.debug "+++++++++++++++++++++++++"
-      # Rails.logger.debug "DestroyAccount.manage_user_initiatives"
-      # Rails.logger.debug "with @user" if @user
-      # Rails.logger.debug @form.to_h
-      # Rails.logger.debug "+++++++++++++++++++++++++"
-
-      Decidim::Initiative.where(author: @user).each do |initiative|
-        # Rails.logger.debug initiative.id
-        # Rails.logger.debug initiative.state
-        if %w[created validating accepted].include?(initiative.state)
-          # Rails.logger.debug "will be discarded"
-          initiative.update_columns(state: 'discarded')
-        else
-          # Rails.logger.debug "will be rejected"
-          initiative.update_columns(state: 'rejected')
-        end
-        # Rails.logger.debug "--"
-      end
-    end
 
     def notify_admins
       organization_admins.each do |admin|
