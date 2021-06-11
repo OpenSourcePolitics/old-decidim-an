6b3bc52e0650d67cdeab7981a55ae23ee86399d5
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2021-06-11 09:47:48 +0200

    Fix/archive filter not displayed (#65)
    * bump mimemagic version
    
    * add archived filter in initiatives view
    
    * add missing fr translation key

--- app/helpers/decidim/initiatives/application_helper.rb
+++ app/helpers/decidim/initiatives/application_helper.rb
@@ -18,7 +18,11 @@ module Decidim
                         TreePoint.new("accepted", t("decidim.initiatives.application_helper.filter_state_values.accepted")),
                         TreePoint.new("rejected", t("decidim.initiatives.application_helper.filter_state_values.rejected"))
                     ]
-                )
+                ),
+                TreeNode.new(
+                    TreePoint.new("archived", t("decidim.initiatives.application_helper.filter_state_values.archived")),
+                    archives_categories_value
+                ),
             ]
         )
       end
@@ -111,6 +115,14 @@ module Decidim
               end
           )
         end
+      end
+
+      def archives_categories_value
+        archive_categories.map { |archive_category| TreePoint.new(archive_category.name, archive_category.name) }
+      end
+
+      def archive_categories
+        @archive_categories ||= Decidim::InitiativesArchiveCategory.where(organization: current_organization)
       end
     end
   end
