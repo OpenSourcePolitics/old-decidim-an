4e6024c571e343b75937010bb8db774f710c22c0
Armand Fardeau <armandfardeau@users.noreply.github.com>
2022-04-25 07:16:46 +0200

    Update rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-class Rack::Attack
-  throttle('req/ip', limit: 100, period: 1.minute) do |req|
-    Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
-    req.ip unless req.path.start_with?('/assets')
+if Rails.env.production? || Rails.env.test?
+  require "rack/attack"
+
+  class Rack::Attack
+    throttle("req/ip", limit: 100, period: 1.minute) do |req|
+      Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
+      req.ip unless req.path.start_with?("/assets")
+    end
   end
 end
